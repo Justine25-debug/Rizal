dbf243cc4b234f756e18a94384cc04906751d742
Justine <justinegasque25@gmail.com>
2026-01-12 06:24:04 +0800

    fix: update base path in Vite configuration for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Rizal/',
   plugins: [react(), tailwindcss()],
+  base: 'https://justine25-debug.github.io/Rizal/',
 })
